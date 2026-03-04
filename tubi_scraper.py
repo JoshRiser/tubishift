7bd59e28f463c8ee1beb1f69bb9731d901b318d7
Josh Riser <riser.josh@gmail.com>
2026-03-04 10:08:30 -0500

    Fixing cookie file url in scraper

--- tubi_scraper.py
+++ tubi_scraper.py
@@ -22,7 +22,7 @@ import re
 from pathlib import Path
 from typing import Optional
 
-COOKIES_FILE = Path(__file__).parent / "cookies.txt"
+COOKIES_FILE = Path(__file__).resolve().parent / "cookies.txt"
 
 HEADERS = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 "
